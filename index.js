6750a9e0df5be29a26fe0bc5d731c773a54c6d86
Michelle Glinkowski <miglinkowski@aol.com>
2023-03-16 00:02:58 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function appendCat(name){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [...cats,name]
+    return newCats;
+}
+appendCat("Broom")
+
+function prependCat(name){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [name,...cats]
+    return newCats;
+}
+prependCat("Bob")
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1) 
+}
